cd0a59c3e4ff25a3baf0880ee1f58874a483394b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2018-08-04 13:45:32 +0200

    Bootstrap styles

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,24 +1,74 @@
 <template>
-    <div v-if="!loading">
-        <header>Words</header>
-        <ul v-if="data.length">
-            <li v-for="word in data" :key="word._id">
-                {{ word }}
+    <div v-if="!loading" class="app-container">
+        <div class="space-between view-header__container">
+			<header class="view-header">Words ready for Memrise</header>
+			<button class="view-header__cta">PREPARE</button>
+		</div>
+		<SortPane :data="data"/>
+        <ul v-if="data.length" class="view-list">
+            <li v-for="word in data" :key="word._id" class="view-list__item">
+                <!-- <Word :word="word"/> -->
             </li>   
         </ul>
-        <span v-else>
+        <div v-else class="no-results">
             No active words...
-        </span>
+        </div>
     </div>
 
 </template>
 
 <script>
+import Word from "./Word";
+import SortPane from "./SortPane";
+
 export default {
 	name: "List",
 	props: ["loading", "data"],
+	components: { Word, SortPane },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "../base.scss";
+.app-container {
+	@include light-border;
+	@include shadow;
+	margin: 2rem auto;
+	max-width: 40rem;
+	width: 100%;
+	min-height: 6rem;
+	background: $almost-white;
+}
+
+.view-header {
+	@include header-font;
+	height: calc(3rem - 1px);
+	padding: calc(0.75rem - 2px) 0 0 1rem;
+	&__container {
+		@include space-between;
+		align-items: center;
+		background: $background;
+	}
+	&__cta {
+		@include btn-cta($bc: $almost-white, $fc: #666);
+		&:hover {
+			border: 1px solid #eee;
+		}
+	}
+}
+
+.view-list {
+	padding: 1rem;
+	&__item {
+		@include light-border;
+		min-height: 5rem;
+		margin-bottom: 1rem;
+	}
+}
+
+.no-results {
+	@include descriptive-text;
+	margin: calc(1rem - 2px) 1rem;
+}
 </style>
+
---
+++ src/components/Word.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Word:
+    </div>
+</template>
+
+<script>
+export default {
+	name: "Word",
+	props: ["word"],
+};
+</script>
+
+<style lang="scss">
+</style>
