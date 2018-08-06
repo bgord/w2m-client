8bf062acddde967207b3a4eab9ca164844faaad0
Bartosz Gordon <bartosz.gordon@sealcode.org>
2018-08-06 11:21:03 +0200

    Word.vue refactor

--- src/components/Word.vue
+++ src/components/Word.vue
@@ -31,7 +31,9 @@
 					    :disabled="editContextPending"
 					    :class="{'btnek':true,'btnek--loading': editContextPending}"
 					    type="submit"
-					>SAVE CONTEXT</button>
+					>
+						SAVE CONTEXT
+						</button>
 				</form>
 			</div>
 			<form @submit.prevent="editTranslation">
@@ -45,7 +47,9 @@
 				    :disabled="editTranslationPending"
 				    :class="{'btnek':true,'btnek--loading': editTranslationPending}"
 				    type="submit"
-				>SAVE TRANSLATION</button>
+				>
+					SAVE TRANSLATION
+					</button>
 			</form>
 			<div
 			    v-if="resource.suggestedTranslations.length"
@@ -72,7 +76,7 @@
 					<div :class="{word__badge:true, blurred: !resource.downloadResult} ">{{ MP3Msg }}</div>
 					<div :class="{word__badge:true, blurred: !resource.suggestedTranslations.length} ">{{suggestionsMsg}}</div>
 				</div>
-				<div>
+				<div class="button-wrapper">
 					<button
 					    v-if="!collapsed"
 					    @click="resetWord"
@@ -243,37 +247,32 @@ export default {
 <style lang="scss">
 @import "../base.scss";
 .word-container {
+	margin-bottom: 1.5rem;
+	padding-bottom: 0.5rem;
+	min-height: 3.5rem;
 	background: #fafafa;
-	border-left: 3px solid $background;
-	border-right: 3px solid $background;
 	font-weight: 600;
 	font-size: 14px;
-	min-height: 3.5rem;
-	padding-bottom: 0.5rem;
-	margin-bottom: 1.5rem;
+	border-left: 3px solid $background;
+	border-right: 3px solid $background;
 }
 .word {
 	display: inline-block;
-	padding-top: 3px;
-	padding-left: 1rem;
-	padding-right: 0.5rem;
+	padding: 3px 0.5rem 0 1rem;
 	&__upper {
-		@include space-between;
-		align-items: baseline;
+		@include space-between($ai: baseline);
 	}
 	&__lower {
-		margin-left: 1rem;
-		margin-top: 0.5rem;
-		height: 1rem;
-		margin-bottom: 0.5rem;
 		@include space-between;
+		margin: 0.5rem 0 0.5rem 1rem;
+		height: 1rem;
 	}
 	&__badge {
 		display: inline-block;
-		background: #eee3ff;
-		padding: 0 0.25rem;
-		color: #ab6efe;
 		margin-right: 0.5rem;
+		padding: 0 0.25rem;
+		background: #eee3ff;
+		color: #ab6efe;
 		font-size: 12px;
 	}
 	&--hightlighted {
@@ -281,12 +280,12 @@ export default {
 	}
 	&__expand {
 		@include btn-reset;
-		height: 1rem;
 		margin-top: 7px;
 		margin-right: 1rem;
-		font-weight: 600;
+		height: 1rem;
 		color: #4286f4;
 		letter-spacing: 1px;
+		font-weight: 600;
 	}
 }
 .blurred {
@@ -295,12 +294,12 @@ export default {
 
 .input {
 	margin: 1rem 1rem 0 1rem;
-	height: 1.75rem;
-	border: 1px solid #eee;
-	width: 20rem;
 	padding-left: 0.5rem;
-	border-radius: 2px;
+	height: 1.75rem;
+	width: 20rem;
+	border: 1px solid #eee;
 	border-left: 2px solid #eee;
+	border-radius: 2px;
 	&--changed {
 		border-left: 2px solid #4286f4;
 	}
@@ -310,12 +309,12 @@ export default {
 	@include btn-cta($bc: #4286f4, $fc: $almost-white);
 	margin-left: 0;
 	height: 1.5rem;
-	border-radius: 3px;
 	font-size: 12px;
 	font-weight: 600;
+	border-radius: 3px;
 	&--loading {
 		opacity: 0.5;
-		box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.3);
+		@include strong-shadow;
 		&:hover {
 			cursor: auto;
 		}
@@ -329,18 +328,18 @@ export default {
 }
 
 .sugg-trans__list {
-	margin-top: 6px;
-	padding-left: 1rem;
-	margin-bottom: 1rem;
-	font-size: 13px;
 	display: inline-block;
+	margin-top: 6px;
+	margin-bottom: 1rem;
+	padding-left: 1rem;
 }
 
 .sugg-trans__item {
-	color: #444;
+	font-size: 13px;
+	color: #555;
 	font-style: italic;
-	cursor: pointer;
 	&:hover {
+		cursor: pointer;
 		opacity: 0.85;
 	}
 }
@@ -351,9 +350,9 @@ export default {
 
 .sugg-trans__text {
 	@include descriptive-text;
-	font-size: 12px;
-	padding-top: 16px;
+	padding-top: 1rem;
 	padding-left: 1rem;
+	font-size: 12px;
 }
 
 .trans {
@@ -364,11 +363,14 @@ export default {
 
 .delete {
 	@include btn-reset;
+	margin: 1px 1rem 0 0;
 	height: 1rem;
-	margin-top: 1px;
-	margin-right: 1rem;
 	font-weight: 600;
 	color: #e54053;
 	letter-spacing: 1px;
+}
+
+.button-wrapper {
+	margin-top: -5px;
 }
 </style>
